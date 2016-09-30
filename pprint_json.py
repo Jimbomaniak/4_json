10b2f4414c4cc75833848ecf3b8e75d05aff5f5e
Olegka <jimboman54321@gmail.com>
2016-09-30 16:07:19 +0300

    add functions to load json file and to pretty print json format

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,21 @@
 import json
 
 
-def load_data(filepath):
-    pass
-
+def load_data():
+    filepath = input('Write your path to directory with json file(like c:/users/bars.json): ')
+    try:
+        with open(filepath) as file:
+            data = json.load(file)
+        return data 
+    except FileNotFoundError:
+        print('No such file in directory')
+        return load_data()
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, ensure_ascii=False, sort_keys=True, indent=4, separators=(',',':')))
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data()
+    pretty_print_json(data)
+    
