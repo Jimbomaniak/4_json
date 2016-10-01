ab4c3b05699f366e59d682d35061e89b5bbbc4d2
Olegka <jimboman54321@gmail.com>
2016-10-01 21:17:42 +0300

    create pure functions

--- pprint_json.py
+++ pprint_json.py
@@ -1,21 +1,22 @@
 import json
 
 
-def load_data():
-    filepath = input('Write your path to directory with json file(like c:/users/bars.json): ')
+def load_data(filepath):
     try:
         with open(filepath) as file:
             data = json.load(file)
         return data 
     except FileNotFoundError:
-        print('No such file in directory')
-        return load_data()
+        return 'No such file in directory'
+
 
 def pretty_print_json(data):
-    print(json.dumps(data, ensure_ascii=False, sort_keys=True, indent=4, separators=(',',':')))
+    pretty_json = json.dumps(data, ensure_ascii=False, sort_keys=True, indent=4, separators=(',',':'))
+    return pretty_json
 
 
 if __name__ == '__main__':
-    data = load_data()
-    pretty_print_json(data)
+    filepath = input('Write your path to directory with json file(like c:/users/bars.json): ')
+    data = load_data(filepath)
+    print(pretty_print_json(data))
     
